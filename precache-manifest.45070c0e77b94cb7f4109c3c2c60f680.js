cf5dd5c7ec571d747f3f847cc75c447d8b9f1e48
rishav <rishav.21m@gmail.com>
2020-03-31 02:31:55 +0530

    Updates

---
+++ precache-manifest.45070c0e77b94cb7f4109c3c2c60f680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae5ab78543a3215e293d199213053f7",
+    "url": "/Drums-2.0/index.html"
+  },
+  {
+    "revision": "c89fd9966492b73da33c",
+    "url": "/Drums-2.0/static/css/main.dfb7cda4.chunk.css"
+  },
+  {
+    "revision": "c72a46be4ee01ab9b68f",
+    "url": "/Drums-2.0/static/js/2.789e862b.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/Drums-2.0/static/js/2.789e862b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c89fd9966492b73da33c",
+    "url": "/Drums-2.0/static/js/main.c6caacf1.chunk.js"
+  },
+  {
+    "revision": "71dde1390e39394b5e9c",
+    "url": "/Drums-2.0/static/js/runtime-main.1f71d430.js"
+  }
+]);
